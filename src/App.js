518526e3c3419a2b26d1eedcad9c7d40b81ab10a
Shashank Biplav <Biplavshashank7@gmail.com>
2020-07-27 23:42:42 +0530

    Defined a person component and injected dynamic content in it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I am a React App</h1>
+      <Person name="Shashank" age="23"/>
+      <Person name="Biplav" age="23"/>
     </div>
   );
 }
