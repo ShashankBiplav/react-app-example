6e0d4816752d52364d0dfdaa2fd5e3ddb2a0d52b
Shashank Biplav <Biplavshashank7@gmail.com>
2020-08-01 20:29:31 +0530

    Conditionally rendering the content

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       { name: 'Shashank', age: 23 },
       { name: 'Biplav', age: 23 },
     ],
+    otherState: 'Some other value',
+    showPersons: false,
   };
 
   switchNameHandler = (newName) => {
@@ -20,7 +22,7 @@ class App extends Component {
     });
   };
 
-  nameChangedHandler =(event)=>{
+  nameChangedHandler = (event) => {
     this.setState({
       persons: [
         { name: 'Shashank', age: 23 },
@@ -29,29 +31,45 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
-    const style = { //inline styling using js objects
+    const style = {
+      //inline styling using js objects
       backgroundColor: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+    };
+    let persons = null;
+
+  if (this.state.showPersons) {
+    persons = (
+      <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              click={this.switchNameHandler.bind(this, 'SHANK!!!!')} //passed method to props
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.nameChangedHandler}
+            />
+          </div>
+    );
+  }
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button style={style}
-        onClick={()=> this.switchNameHandler('SHANKY')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click = {this.switchNameHandler.bind(this, 'SHANK!!!!')} //passed method to props
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          changed={this.nameChangedHandler}
-        />
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch visibility
+        </button>
+         {persons}
       </div>
     );
   }
