c34cb41d6d1b1fec114759041f542a650afdc1f0
Shashank Biplav <Biplavshashank7@gmail.com>
2020-08-01 22:11:52 +0530

    Made list flexible

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Shashank', age: 23 },
-      { name: 'Biplav', age: 23 },
+      {id:'1', name: 'Shashank', age: 23 },
+      {id:'2', name: 'Biplav', age: 23 },
     ],
     otherState: 'Some other value',
     showPersons: false,
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex]= person;
     this.setState({
-      persons: [
-        { name: 'Shashank', age: 23 },
-        { name: event.target.value, age: 22 },
-      ],
+      persons: persons
     });
   };
 
@@ -25,7 +29,7 @@ class App extends Component {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons:persons});
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -48,8 +52,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person click={()=>this.deletePersonHandler(index)}
-            name={person.name} age={person.age} />;
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed ={(event)=>this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
