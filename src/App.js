c4d3323d7f49434f188007ea0f46581226ed3fd6
Shashank Biplav <Biplavshashank7@gmail.com>
2020-08-01 20:38:39 +0530

    Outputting lists by mapping to jsx

--- src/App.js
+++ src/App.js
@@ -47,29 +47,22 @@ class App extends Component {
     };
     let persons = null;
 
-  if (this.state.showPersons) {
-    persons = (
-      <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'SHANK!!!!')} //passed method to props
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              changed={this.nameChangedHandler}
-            />
-          </div>
-    );
-  }
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person) => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <button style={style} onClick={this.togglePersonsHandler}>
           Switch visibility
         </button>
-         {persons}
+        {persons}
       </div>
     );
   }
